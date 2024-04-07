5c722151b4c35be2e0f1f48e449730b23623e10f
karesiak <kordek0945@gmail.com>
2024-04-07 23:23:21 +0200

    66

---
+++ formularz/Controllers/OsobaController.cs
@@ -0,0 +1,47 @@
+﻿using formularz.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace formularz.Controllers
+{
+    public class OsobaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        public ActionResult Index(Osoba osoba)
+        {
+            if (ModelState.IsValid)
+            {
+                // Zapisanie danych z formularza do TempData
+                TempData["Osoba"] = osoba;
+                return RedirectToAction("ZapiszOsobe");
+            }
+            return View(osoba);
+        }
+
+
+        public ActionResult ZapiszOsobe()
+        {
+            // Pobranie obiektu Osoba z TempData
+            Osoba osoba = TempData["Osoba"] as Osoba;
+
+            // Sprawdzamy, czy obiekt faktycznie istnieje
+            if (osoba != null)
+            {
+                // Przekazanie obiektu Osoba do widoku
+                return View(osoba);
+            }
+            else
+            {
+                // Przekierowanie z powrotem do formularza, jeśli nie ma danych
+                return RedirectToAction("Index");
+            }
+        }
+
+
+    }
+}
---
+++ formularz/Models/Osoba.cs
@@ -0,0 +1,9 @@
+﻿namespace formularz.Models
+{
+    public class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Email { get; set; }
+    }
+}
